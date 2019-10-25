8bd847ab416ad44b7feee2c16d360de59a09dbf2
k.maslowski <k.maslowski@groupeonepoint.com>
2019-10-25 10:42:02 +0200

    V1.1

--- src/app/model/gymnases.model.ts
+++ src/app/model/gymnases.model.ts
@@ -5,6 +5,17 @@ export class GymnasesAll {
         public nomGymnase: string,
         public adresse: string,
         public ville: string,
+        public surface: number
+    ) {}
+}
+
+export class GymnaseDetail {
+    constructor(
+        public _id: string,
+        public idGymnase: number,
+        public nomGymnase: string,
+        public adresse: string,
+        public ville: string,
         public surface: number,
     ) {}
 }
