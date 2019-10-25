6de19566b3f18d96a31883a59eceac2d5cbb2f45
k.maslowski <k.maslowski@groupeonepoint.com>
2019-10-25 17:12:21 +0200

    K-01

--- src/app/services/gymnasess.service.ts
+++ src/app/services/gymnasess.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
-import { GymnasesAll, GymnaseDetail } from '../model/gymnases.model';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { GymnasesAll, GymnaseDetail, Seances } from '../model/gymnases.model';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-type': 'application/json' })
@@ -11,7 +10,7 @@ const httpOptions = {
 @Injectable({providedIn: 'root'})
 export class GymnasessService {
 
-  readonly urlGymnases = 'http://http://localhost:8080/sportifs';
+  readonly urlGymnases = 'http://localhost:8080/gymnases';
 
   constructor(private httpService: HttpClient) { }
 
@@ -21,9 +20,46 @@ export class GymnasessService {
     return this.httpService.get<GymnasesAll>(url);
   }
 
-  getGymnase(_id: string): Observable<GymnaseDetail> {
+  getGymnaseById(id: string): Observable<GymnaseDetail> {
     console.log('Call getGymnase');
-    const url = `${this.urlGymnases}/${_id}`;
+    const url = `${this.urlGymnases}/id/${id}`;
     return this.httpService.get<GymnaseDetail>(url);
   }
+
+  getNamesGymnases(): Observable<String> {
+    console.log('Call getNamesGymnase');
+    const url = `${this.urlGymnases}/noms`;
+    return this.httpService.get<String>(url);
+  }
+
+  getGymnaseByName(nom: string): Observable<GymnaseDetail> {
+    console.log('Call getGymnaseByName');
+    const url = `${this.urlGymnases}/nom/${nom}`;
+    return this.httpService.get<GymnaseDetail>(url);
+  }
+
+  getGymnaseWithSurface(min: number, max: number): Observable<GymnaseDetail> {
+    console.log('Call getGymnaseWithSurface');
+    const url = `${this.urlGymnases}/surface-min-max` + min + max;
+    return this.httpService.get<GymnaseDetail>(url);
+  }
+
+  getAllGymnaseSurface(): Observable <number> {
+    console.log('Call getAllGymnaseSurface');
+    const url = `${this.urlGymnases}/surface`;
+    return this.httpService.get<number>(url);
+  }
+
+  getSeancesById(id: number): Observable <Seances> {
+    console.log('Call getSeancesById');
+    const url = `${this.urlGymnases}/seances/${id}`;
+    return this.httpService.get<Seances>(url);
+  }
+
+  getSeancesEntrainerById(id: number): Observable <Seances> {
+    console.log('Call getSeancesEntrainerById');
+    const url = `${this.urlGymnases}/seances-entraineur/${id}`;
+    return this.httpService.get<Seances>(url);
+  }
+
 }
